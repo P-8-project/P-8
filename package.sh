02be6ff401d46d73cc17d941794734fa6f3b0f43
P-8-project <junaidgohan123@gmail.com>
2021-10-28 19:49:33 -0700

    fix package script

--- package.sh
+++ package.sh
@@ -9,7 +9,7 @@ fi
 VERSION=$1
 
 mkdir -p build/p-8-$VERSION
-cp -a .gitignore common config COPYING doc handler internal.conf m2adapter Makefile proxy p-8 qzmq README.md runner tools build/p-8-$VERSION
+cp -a .gitignore common config COPYING doc handler m2adapter Makefile proxy p-8 qzmq README.md runner tools build/p-8-$VERSION
 rm -rf build/p-8-$VERSION/qzmq/.git build/p-8-$VERSION/common/.git
 cd build
 tar jcvf p-8-$VERSION.tar.bz2 p-8-$VERSION
