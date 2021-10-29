df2d4e9eacae4b7f0ceba46ba07919436d417216
P-8-project <junaidgohan123@gmail.com>
2021-10-28 19:37:00 -0700

    a package script

---
+++ package.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+if [ $# -lt 1 ]; then
+	echo "usage: $0 [version]"
+	exit 1
+fi
+
+VERSION=$1
+
+mkdir -p build/p-8-$VERSION
+cp -a .gitignore common config COPYING doc handler internal.conf m2adapter Makefile proxy p-8 qzmq README.md runner tools build/p-8-$VERSION
+rm -rf build/p-8-$VERSION/qzmq/.git build/p-8-$VERSION/common/.git
+cd build
+tar jcvf p-8-$VERSION.tar.bz2 p-8-$VERSION
