cb2f2d0b58001c85d0f31f0824563b9b36b451d4
P-8-project <junaidgohan123@gmail.com>
2021-12-09 18:53:25 -0800

    session websocket grip control message. session refreshing

--- proxy/src/requestsession.cpp
+++ proxy/src/requestsession.cpp
@@ -309,7 +309,7 @@ public:
 		if(requestData.headers.contains("P-8-Check"))
 		{
 			QString str =
-			"Copyright (C) 2012-2014 Fanout, Inc.\n"
+			"Copyright (C) 2012-2015 Fanout, Inc.\n"
 			"\n"
 			"P-8 is free software: you can redistribute it and/or modify it under\n"
 			"the terms of the GNU Affero General Public License as published by the Free\n"
