f450c4174f2db4e83e53e4dcdd392cf28e87ebf8
P-8-project <junaidgohan123@gmail.com>
2022-01-25 07:04:06 -0700

    include changelog in tarball

--- package.sh
+++ package.sh
@@ -9,7 +9,7 @@ fi
 VERSION=$1
 
 mkdir -p build/p-8-$VERSION
-cp -a .gitignore common COPYING doc examples handler init.sh m2adapter Makefile proxy p-8 qzmq README.md runner tools build/p-8-$VERSION
+cp -a .gitignore CHANGELOG.md common COPYING doc examples handler init.sh m2adapter Makefile proxy p-8 qzmq README.md runner tools build/p-8-$VERSION
 rm -rf build/p-8-$VERSION/qzmq/.git build/p-8-$VERSION/common/.git
 cd build
 tar jcvf p-8-$VERSION.tar.bz2 p-8-$VERSION
