eb5009c669bd134db7eccb3ebb654dec8e7a4ceb
P-8-project <junaidgohan123@gmail.com>
2022-04-09 15:02:20 -0800

    update package script

--- package.sh
+++ package.sh
@@ -10,7 +10,7 @@ VERSION=$1
 
 mkdir -p build/p-8-$VERSION
 cp -a .gitignore CHANGELOG.md COPYING corelib doc examples handler init.sh m2adapter Makefile proxy p-8 README.md runner tools build/p-8-$VERSION
-rm -rf build/p-8-$VERSION/qzmq/.git build/p-8-$VERSION/common/.git
+rm -rf build/p-8-$VERSION/corelib/qzmq/.git build/p-8-$VERSION/corelib/common/.git
 echo $VERSION > build/p-8-$VERSION/version
 cd build
 tar jcvf p-8-$VERSION.tar.bz2 p-8-$VERSION
