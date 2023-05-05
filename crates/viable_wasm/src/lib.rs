f58bb1a166257b1eaeb0a9cb71630d5a2c0a76e6
new-username12 <junaidgohan123@gmail.com>
2023-05-05 06:57:28 +0100

    fix qualifier

--- crates/viable_wasm/src/lib.rs
+++ crates/viable_wasm/src/lib.rs
@@ -2,5 +2,5 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn compiler(source: &str) -> Result<String, JsValue> {
-    Ok(viable::compiler(&source).expect_throw("Encountered a parsing error"))
+    Ok(viable_compiler::compiler(&source).expect_throw("Encountered a parsing error"))
 }
