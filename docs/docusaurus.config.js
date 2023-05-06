6218d6638b0383596c31ddb38827a42fc7dcebff
new-username12 <junaidgohan123@gmail.com>
2023-05-06 21:55:58 +0100

    update navbar

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
         items: [
           {
             href: "/docs/install",
-            label: "Docs",
+            label: "Viable Documentation",
             position: "left",
           },
           {
