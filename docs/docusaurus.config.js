d087bb3eb082b7a42148c439e54be4f8f789f203
new-username12 <junaidgohan123@gmail.com>
2023-05-10 12:04:39 +0100

    fix default doc

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -35,7 +35,7 @@ const config = {
       navbar: {
         items: [
           {
-            href: "/docs/install",
+            href: "/docs/welcome",
             label: "Viable Documentation",
             position: "left",
           },
