934cede3d60fec7aa0181a35406657d33c65a688
new-username12 <junaidgohan123@gmail.com>
2023-05-23 07:06:02 +0100

    cleanup

--- crates/viable_compiler/fuzz/fuzz_targets/ast_fuzz.rs
+++ crates/viable_compiler/fuzz/fuzz_targets/ast_fuzz.rs
@@ -1,7 +1,6 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use viable_compiler::ast::enums::*;
-use viable_compiler::to_regex;
+use viable_compiler::{ast::enums::*, to_regex};
 
 fuzz_target!(|data: Vec<Node>| {
     drop(to_regex(&data));
