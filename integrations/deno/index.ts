4ed701768eeb4b58ec55b5ee9aca8683dd8b10d9
new-username12 <junaidgohan123@gmail.com>
2023-06-13 21:17:26 +0200

    update deno intergration

--- integrations/deno/index.ts
+++
@@ -1,8 +0,0 @@
-import init, { compiler as viablec } from "./wasm/viable_wasm.js";
-
-const initialized = init(Deno.readFile("./wasm/viable_wasm_bg.wasm"));
-
-export async function compiler(source: string): Promise<string> {
-  await initialized;
-  return viablec(source);
-}
